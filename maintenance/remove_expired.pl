1fda178be47c864ea01fc5107d31651eaf2b0d87
Jeremy Clerc <jeremy@clerc.io>
2014-07-06 20:37:02 +0200

    Less output on maintenance script

--- maintenance/remove_expired.pl
+++ maintenance/remove_expired.pl
@@ -8,13 +8,12 @@ if ($#ARGV ne 0) {
     print "Usage: $0 path_to_data_dir/";
     exit 1;
 }
-
-print "Cleaning directory $ARGV[0] ...\n";
+print ".";
 foreach my $file (glob $ARGV[0] . "/*") {
     if ($file =~ /$ARGV[0]\/([a-zA-Z0-9]+)-[a-z0-9]+-([0-9]+)/) {
         if ($2 < $curtime) {
             unlink $file;
-            print "Cleaning paste $1\n";
+            print "\n" . localtime($curtime) . " Cleaning paste $1";
         }
     }
 }
